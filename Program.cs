ba596aea0a16b6511bc45f66c2d73b16ff62c3a2
hakue1990 <48098101+hakue1990@users.noreply.github.com>
2020-02-17 21:54:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Descending_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DesscendingOrder(123456);
+                Console.ReadKey();
+        }
+        public static void DesscendingOrder(int num)
+        {
+            char[] array = num.ToString().ToCharArray();
+            Array.Reverse(array);
+            int newSort = Convert.ToInt32(new string(array));
+            Console.WriteLine(newSort);
+        }
+    }
+}
