6033486573d7b3c59c19ffe1afe9670babb90769
hakue1990 <48098101+hakue1990@users.noreply.github.com>
2020-02-18 21:38:07 +0100

    Update Program.cs

--- getVovelsNumber/Program.cs
+++ getVovelsNumber/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//Return the number (count) of vowels in the given string.
+
+//We will consider a, e, i, o, and u as vowels for this Kata.
+
+//The input string will only consist of lower case letters and/or spaces.
+
 namespace vovelsNumber
 {
     class Program
