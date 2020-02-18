8ccaf99cd91ecd2511a3699a19638eaedf668591
hakue1990 <48098101+hakue1990@users.noreply.github.com>
2020-02-18 15:59:27 +0100

    Add files via upload

---
+++ getVovelsNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vovelsNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetVovelCount("eease");
+            Console.ReadKey();
+        }
+        public static void GetVovelCount(string str)
+        {
+            Console.WriteLine(str.Count(i => "eaoie".Contains(i)));
+            
+        }
+    }
+}
