9b2aa51c82e5476b20a4044297605aa63a4193eb
hakue1990 <48098101+hakue1990@users.noreply.github.com>
2020-02-18 21:38:52 +0100

    Update Program.cs

--- DescendingOrder/Program.cs
+++ DescendingOrder/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+//Examples: Input: 21445 Output: 54421
+
+//Input: 145263 Output: 654321
+
+//Input: 123456789 Output: 987654321
+
 namespace Descending_Order
 {
     class Program
